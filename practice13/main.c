290672b5db21cf69aca55aed045948e408878054
jonghoinside <84114844+jonghoinside@users.noreply.github.com>
2021-06-20 23:09:38 +0900

    [프로그래머스] 스킬 트리 문제 중간 풀이 커밋

--- practice13/main.c
+++ practice13/main.c
@@ -7,6 +7,8 @@
 int solution(const char* skill, const char* skill_trees[], size_t skill_trees_len);
 
 int main(void) {
+    char skill_trees[4][5] = {"BACDE", "CBADF", "AECB", "BDA"};
+    printf("%d\n", solution("CBD",skill_trees, 4));
 
     return 0;
 }
--- practice13/solution.c
+++ practice13/solution.c
@@ -11,29 +11,18 @@
 int solution(const char* skill, const char* skill_trees[], size_t skill_trees_len) {
     char arr[26] = {0, };
 
-    for (int i = 0; skill_trees[i]; ++i) {
+    for (int i = 0; skill_trees_len; ++i) {
         int count = 0;
-        for (int j = 0; skill_trees[i][j]; ++j) {
+        for (int j = 0; skill_trees[i]; ++j) {
             for (int k = 0; skill[k]; ++k) {
                 if (skill_trees[i][j] == skill[k])
                     arr[count++] = skill_trees[i][j];
             }
         }
-
-
-
-
-
-
     }
 
-    if(skill[0] == arr[0])
-
-
-    for (int i = 0; i < count; ++i)
-
-
-
+    for (int i = 0; arr[i]; ++i)
+        printf("%d\n", arr[i]);
 
     int answer = -1;
     return answer;
